6d9190e41a0e48c839633f982bda56887e06cad3
joaolflores-ui <joao_l_flores@estudante.sesisenai.org.br>
2026-02-19 22:04:05 -0300

    terceiraaula

---
+++ modelos.py
@@ -0,0 +1,16 @@
+"""
+Atributos da fabrica de chocolate
+Atributo1 -_CPF
+Atributo2-endereço
+Atributo3-Idade
+Atributo4-recorrencia
+Atributo5-fidelização
+
+
+Comportamentos:
+Cancelar consulta
+Devolver
+Compartilhar
+•  Experimentar
+•  Reclamar
+"""
